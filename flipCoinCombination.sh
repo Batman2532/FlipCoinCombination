3b480c1c2ac7ccbe40226ef769a197a890899dbe
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-09 13:12:44 +0530

    Added: Determine the percentage of the singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,19 @@
-#!/bin/bash
-
-result=$((RANDOM%2))
-if [ $result -eq 0 ]
-then
-	echo Heads win
-else
-	echo Tails win
-fi
+#!/bin/bash -x
+declare -A flippingCoin
+flippingCoin[heads]=0
+flippingCoin[tails]=0
+counter=0
+while [ $counter -lt 20 ]
+do
+	result=$((RANDOM%2))
+	if [ $result -eq 0 ]
+	then
+		flippingCoin[heads]=$(( ${flippingCoin[heads]} + 1 ))
+	else
+		flippingCoin[tails]=$(( ${flippingCoin[tails]} + 1 ))
+	fi
+	((counter++))
+done
+headsPercentage=$((${flippingCoin[heads]} * 100 / 20 )) 
+tailsPercentage=$((${flippingCoin[tails]} * 100 / 20 ))
+echo  "Head percentage is: "$headsPercentage"%" "Tails percentage is: " $tailsPercentage"%"
